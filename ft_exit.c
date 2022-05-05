a1fdb8f008a677f428d0f2047d6fd401a384ee1c
Jaewoo Park <jaewpark@c5r6s3.42seoul.kr>
2022-05-05 19:04:13 +0900

    builtin: exit #1st complete

--- ft_exit.c
+++ ft_exit.c
@@ -1,9 +1,13 @@
 #define SUCCESS 0
 #define FAIL 1
+#include <unistd.h>
+
+// STDERR_FILENO 2
 
 typedef struct s_ast
 {
-	char *content;
+	char *content; 
+    void *prev;
 	void *next;
 }	t_ast;
 
@@ -16,26 +20,46 @@ static int	change_to_number(char *str)
     ret = 0;
     if (*str == '-' && *str++)
         flag = -1;
-    if ('0' > *str && *str > '9')
-        return 0;
     while (*str && ('0' <= *str && *str <= '9'))
 		ret = (ret * 10) + *str++ - '0';
 	return (ret * flag);
 }
 
-static unsigned char return_value(int num)
+static int  is_number(char *str)
 {
-    return num;
+    while (*str)
+    {
+        if (*str < '0' && *str > '9')
+            return (1);
+        *str++;
+    }    
+    return (0);
 }
 
-int check_arg(t_ast *ast)
+static int check_arg(t_ast *ast)
 {
-    if (ast->content == '0')
+	unsigned char   exit_code;
+    char	        *str;
+
+	str = ast->content;
+	if (is_number(*str))
+	{
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(ast->content, STDERR_FILENO);
+		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+		exit(255);
+	}
+    else
     {
-        printf("0\n");
-        return (0);
+        exit_code = change_to_number(str);
+        if (ast->next)
+        {
+            ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+		    return (1);
+        }
+        exit(exit_code);
     }
-    return (change_to_number(ast->content));
+	return (0);
 }
 
 int ft_exit(t_ast *ast)
